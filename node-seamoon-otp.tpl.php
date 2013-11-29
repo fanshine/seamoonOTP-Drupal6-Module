a6fcb2dd032868ec38bdea1155503fb5f7f955e8
xdemocle <xdemocle@gmail.com>
2013-11-29 19:45:02 +0100

    initial commit

---
+++ node-seamoon-otp.tpl.php
@@ -0,0 +1,9 @@
+<div class="node<?php if ($type) { print " node-".$type; } ?>">
+  <div class="top"><h2 class="title"><?php print $title?></h2></div>      
+  <div class="wrap">
+    <div class="content">
+      <?php print $content?>
+    </div>
+  </div>
+  <div class="bottom"></div>
+</div>
